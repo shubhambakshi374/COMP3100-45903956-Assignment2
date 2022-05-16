bab4541285cf26c36ee959bda77855ecee14f3b5
shubham.bakshi <shubham.bakshi@prospecta.com>
2022-05-16 23:00:48 +1000

    Setup

---
+++ TCPClient.java
@@ -0,0 +1,12 @@
+import java.net.Socket;
+
+/**
+ * TCPClient
+ */
+public class TCPClient {
+
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("localhost", 50000);
+        
+    }
+}
