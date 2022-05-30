e9d03bfe61ad3a3fd53e6e22cb5e3aaf9f6c5092
shubham.bakshi <shubham.bakhsi@students.mq.edu.au>
2022-05-30 14:48:05 +1000

    major changes v2

--- Assignment2.java
+++ Assignment2.java
@@ -3,12 +3,9 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-// import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.HashMap;
-// import java.util.HashSet;
 import java.util.List;
-// import java.util.Set;
-// import java.util.stream.Collectors;
 
 /**
  * TCPClient
@@ -19,7 +16,9 @@ public class Assignment2 {
   static HashMap<Integer, List<Server>> serverMap = new HashMap<Integer, List<Server>>();
   static Socket socket = null;
   static DataOutputStream dout = null;
-
+  static List<Server> allServers = new ArrayList<Server>();
+  static HashMap<Job, Server> jobMap = new HashMap<Job, Server>();
+  static List<Job> allJobs = new ArrayList<Job>();
 
   public static void main(String[] args) throws Exception {
     socket = new Socket("localhost", 50000);
@@ -36,6 +35,47 @@ public class Assignment2 {
       respString = new BufferedReader(new InputStreamReader(socket.getInputStream())).readLine();
     }
 
+    reqString = "REDY";
+    dout.write((reqString + "\n").getBytes());
+    dout.flush();
+    respString = new BufferedReader(new InputStreamReader(socket.getInputStream())).readLine();
+
+    if(respString.contains("JOBN")) {
+      Job job = parseJob(respString);
+      allJobs.add(job);
+      reqString = "GETS All";
+      dout.write((reqString + "\n").getBytes());
+      dout.flush();
+      respString = new BufferedReader(new InputStreamReader(socket.getInputStream())).readLine();
+      NOS = Integer.parseInt(respString.split(" ")[1]);
+
+      // send ok
+      reqString = "OK";
+      dout.write((reqString + "\n").getBytes());
+      dout.flush();
+      BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      addAllServers(reader);
+      
+      // ack
+      reqString = "OK";
+      dout.write((reqString + "\n").getBytes());
+      dout.flush();
+      respString = new BufferedReader(new InputStreamReader(socket.getInputStream())).readLine();
+      
+      Server server = getAvailableServer(job);
+      jobMap.put(job, server);
+      // send ok
+      reqString = "OK";
+      dout.write((reqString + "\n").getBytes());
+      dout.flush();
+      respString = new BufferedReader(new InputStreamReader(socket.getInputStream())).readLine();
+
+      //Schedule a job
+      reqString = "SCHD " + job.getJobID() + " " + server.getServerType() + " " + server.getServerID();
+      dout.write((reqString + "\n").getBytes());
+      dout.flush();
+      respString = new BufferedReader(new InputStreamReader(socket.getInputStream())).readLine();
+    }
     while(true) {
       reqString = "REDY";
       dout.write((reqString + "\n").getBytes());
@@ -43,15 +83,13 @@ public class Assignment2 {
       respString = new BufferedReader(new InputStreamReader(socket.getInputStream())).readLine();
       if(respString.contains("JOBN")) {
         Job job = parseJob(respString);
-        Server server = getCapableServers(job);
-
-
-        // send ok
-        reqString = "OK";
-        dout.write((reqString + "\n").getBytes());
-        dout.flush();
-        respString = new BufferedReader(new InputStreamReader(socket.getInputStream())).readLine();
-
+        allJobs.add(job);
+       
+        Server server = getAvailableServer(job);
+        if(server == null) {
+          server = getCapableServer(job);
+        }
+        jobMap.put(job, server);
         //Schedule a job
         reqString = "SCHD " + job.getJobID() + " " + server.getServerType() + " " + server.getServerID();
         dout.write((reqString + "\n").getBytes());
@@ -59,6 +97,8 @@ public class Assignment2 {
         respString = new BufferedReader(new InputStreamReader(socket.getInputStream())).readLine();
       } else if(respString.contains("NONE")) {
         break;
+      } else if(respString.contains("JCPL")) {
+        updateServer(respString.split(" ")[2]);
       }
     }
 
@@ -69,38 +109,79 @@ public class Assignment2 {
     socket.close();
   }
 
-  public static Server getCapableServers(Job job) throws IOException {
-    String reqString = "GETS Capable " + String.valueOf(job.getJobCore()) + " " + String.valueOf(job.getJobMem()) + " " + String.valueOf(job.getJobDisk());
-    dout.write((reqString + "\n").getBytes());
-    dout.flush();
-    String respString = new BufferedReader(new InputStreamReader(socket.getInputStream())).readLine();
-    NOS = Integer.parseInt(respString.split(" ")[1]);
-    if(NOS > 0) {
-      reqString = "OK";
-      dout.write((reqString + "\n").getBytes());
-      dout.flush();
-      BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      // System.out.println(reader.readLine());
-      return getBestServer(reader);
-    } else {
-      reqString = "OK";
-      dout.write((reqString + "\n").getBytes());
-      dout.flush();
-      getCapableServers(job);
+  public static void addAllServers(BufferedReader reader) throws IOException {
+    for(int i = 0; i < NOS; i++) {
+      String serverLine = reader.readLine();
+      Server server = serverInfo(serverLine);
+      allServers.add(server);
     }
-    return null;
   }
 
-  public static Server getBestServer(BufferedReader reader) throws IOException {
+  // que in server with sufficient cores
+  public static Server getAvailableServer(Job job) {
     Server server = new Server();
-    for(int i=0; i < NOS; i++) {
-      String line = reader.readLine();
-      server = serverInfo(line);
-      if(server.getServerState().equalsIgnoreCase("booting") || server.getServerState().equalsIgnoreCase("active") || server.getServerState().equalsIgnoreCase("idle")) {
-        return server;
+    int index = 0;
+    boolean flag = false;
+    for(int i = 0; i < allServers.size(); i++) {
+      if(allServers.get(i).getAvailableCores() >= job.getJobCore()) {
+        if(allServers.get(i).getServerState().equalsIgnoreCase("inactive") && !flag) {
+          server = allServers.get(i);
+          flag = true;
+        } else if(allServers.get(i).getServerState().equals("active")) {
+          server = allServers.get(i);
+          server.setAvailableCores(server.getAvailableCores() - job.getJobCore());
+          allServers.set(i, server);
+          return server;
+        }
       }
     }
+    if(flag) {
+      server.setAvailableCores(server.getAvailableCores() - job.getJobCore());
+      server.setServerState("active");
+      allServers.set(index, server);
+      return server;
+    } else {
+      return null;
+    } 
+  }
+
+  // in case no sufficient cores found
+  public static Server getCapableServer(Job job) {
+    Server server = new Server();
+    int index = 0;
+    for(int i = 0; i < allServers.size(); i++) {
+      if(job.getJobCore() <= allServers.get(i).getServerCore()) {
+        if(allServers.get(i).getServerState().equals("inactive")) {
+          server = allServers.get(i);
+          index = i;
+        } else if(allServers.get(i).getServerState().equals("active")) {
+          server = allServers.get(i);
+          return server;
+        }
+      }
+    }
+    server.setServerState("active");
+    allServers.set(index, server);
     return server;
+  }
+
+  // whem jcpl recieved
+  public static void updateServer(String jobId) {
+    Job rJob = new Job();
+    for(Job job: allJobs) {
+      if(job.getJobID() == Integer.parseInt(jobId)) {
+        rJob = job;
+        break;
+      }
+    }
+    Server server = jobMap.get(rJob);
+    for(int i = 0; i < allServers.size(); i++) {
+      if(server.getServerType().equals(allServers.get(i).getServerType()) && server.getServerID() == allServers.get(i).getServerID()) {
+        server.setAvailableCores(server.getAvailableCores() + rJob.getJobCore());
+        allServers.set(i, server);
+        break;
+      }
+    }
   }
 
   public static Job parseJob(String jobString) {
@@ -116,6 +197,7 @@ public class Assignment2 {
   }
 
   public static Server serverInfo(String serverString) {
+    System.out.println(serverString);
     String[] serverInfoArray = serverString.split(" ");
     Server server = new Server();
     server.setServerType(serverInfoArray[0]);
@@ -123,6 +205,7 @@ public class Assignment2 {
     server.setServerState(serverInfoArray[2]);
     server.setServerStartTime(serverInfoArray[3]);
     server.setServerCore(Integer.parseInt(serverInfoArray[4]));
+    server.setAvailableCores(Integer.parseInt(serverInfoArray[4]));
     server.setServerMemory(Integer.parseInt(serverInfoArray[5]));
     server.setServerWJobs(Integer.parseInt(serverInfoArray[6]));
     server.setServerRJobs(Integer.parseInt(serverInfoArray[7]));
@@ -192,20 +275,21 @@ class Job {
 class Server {
 
   private String serverType;
-  private int serverID;
+  private Integer serverID;
   private String serverState;
   private String serverStartTime;
-  private int serverCore;
-  private int serverMemory;
-  private int serverDisk;
-  private int serverWJobs;
-  private int serverRJobs;
+  private Integer serverCore;
+  private Integer serverMemory;
+  private Integer serverDisk;
+  private Integer serverWJobs;
+  private Integer serverRJobs;
+  private Integer availableCores;
 
   public void setServerType(String serverType) {
     this.serverType = serverType;
   }
 
-  public void setServerID(int serverID) {
+  public void setServerID(Integer serverID) {
     this.serverID = serverID;
   }
 
@@ -217,31 +301,35 @@ class Server {
     this.serverStartTime = serverStartTime;
   }
 
-  public void setServerCore(int serverCore) {
+  public void setServerCore(Integer serverCore) {
     this.serverCore = serverCore;
   }
 
-  public void setServerMemory(int serverMemory) {
+  public void setServerMemory(Integer serverMemory) {
     this.serverMemory = serverMemory;
   }
 
-  public void setServerDisk(int serverDisk) {
+  public void setServerDisk(Integer serverDisk) {
     this.serverDisk = serverDisk;
   }
 
-  public void setServerWJobs(int serverWJobs) {
+  public void setServerWJobs(Integer serverWJobs) {
     this.serverWJobs = serverWJobs;
   }
 
-  public void setServerRJobs(int serverRJobs) {
+  public void setServerRJobs(Integer serverRJobs) {
     this.serverRJobs = serverRJobs;
+  }
+
+  public void setAvailableCores(Integer availableCores) {
+    this.availableCores = availableCores;
   }
 
   public String getServerType() {
     return serverType;
   }
 
-  public int getServerID() {
+  public Integer getServerID() {
     return serverID;
   }
 
@@ -253,24 +341,28 @@ class Server {
     return serverStartTime;
   }
 
-  public int getServerCore() {
+  public Integer getServerCore() {
     return serverCore;
   }
 
-  public int getServerMemory() {
+  public Integer getServerMemory() {
     return serverMemory;
   }
 
-  public int getServerDisk() {
+  public Integer getServerDisk() {
     return serverDisk;
   }
 
-  public int getServerWJobs() {
+  public Integer getServerWJobs() {
     return serverWJobs;
   }
 
-  public int getServerRJobs() {
+  public Integer getServerRJobs() {
     return serverRJobs;
+  }
+
+  public Integer getAvailableCores() {
+    return availableCores;
   }
 
   public String toString() {
@@ -295,4 +387,5 @@ class Server {
       String.valueOf(serverRJobs)
     );
   }
+
 }
